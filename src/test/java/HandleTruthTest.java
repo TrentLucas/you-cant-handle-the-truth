9de66887bf405fd605a840c15149c040e642a4fd
TrentLucas <itsstatiic@gmail.com>
2020-11-19 21:22:50 -0800

    strategy written as a comment in the test code

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -6,3 +6,10 @@ public class HandleTruthTest {
         HandleTruth.wordCount();
     }
 }
+/*
+testcases: empty, 1 word, many words, same words but one with capital and one not, punctuation, misspelled words,
+- will need to change the code to accept a input so that testcases can be used
+- store each testcase in a string[] with expected outputs
+- call the function
+- compare the called function output to the string[]
+ */
